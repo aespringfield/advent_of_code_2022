bcf44f69315c8052405f0e8b99fd6c9f6bea9601
Anna Springfield <anna@procurated.com>
2022-12-04 14:34:04 -0600

    Through Day 4

--- spec/01_spec.rb
+++ spec/01_spec.rb
@@ -1,13 +1,13 @@
 describe 'Day01' do
   describe '.part_one' do
     it 'produces the correct output' do
-      expect(Day01.part_one(@input)).to eq(24_000)
+      expect(Day01.part_one(@input)).to eq(@part_one_expected)
     end
   end
 
   describe '.part_two' do
     it 'produces the correct output' do
-      expect(Day01.part_two(@input)).to eq(45_000)
+      expect(Day01.part_two(@input)).to eq(@part_two_expected)
     end
   end
 end
--- spec/02_spec.rb
+++ spec/02_spec.rb
@@ -1,13 +1,13 @@
 describe 'Day02' do
   describe '.part_one' do
     it 'produces the correct output' do
-      expect(Day02.part_one(@input)).to eq(15)
+      expect(Day02.part_one(@input)).to eq(@part_one_expected)
     end
   end
 
   describe '.part_two' do
     it 'produces the correct output' do
-      expect(Day02.part_two(@input)).to eq(12)
+      expect(Day02.part_two(@input)).to eq(@part_two_expected)
     end
   end
 end
